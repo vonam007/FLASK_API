91265fddc0e44deb71dd131946f295f81ff62e41
Võ Nguyễn Hoài Nam <vonguyenhoainam123@gmail.com>
2023-12-12 20:26:57 +0700

    update color difference for bdbox

--- CLIENT/js/drawBoxesOnImage.js
+++ CLIENT/js/drawBoxesOnImage.js
@@ -19,24 +19,36 @@ export async function drawBoxesOnImage(fileJson) {
       // Vẽ hình ảnh lên canvas
       ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-
+      function getRandomColor() {
+          const letters = '0123456789ABCDEF';
+          let color = '#';
+          for (let i = 0; i < 6; i++) {
+              color += letters[Math.floor(Math.random() * 16)];
+          }
+          return color;
+      }
       jsonData.forEach(function (item) {
         let label = item.label;
         let score = item.score;
         let box = item.box;
 
+
+        let colorx = getRandomColor();
         // Vẽ hộp quanh vật thể
         ctx.beginPath();
         ctx.rect(box[0], box[1], box[2] - box[0], box[3] - box[1]);
         ctx.lineWidth = 2;
-        ctx.strokeStyle = 'red';
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.1)';
+        ctx.strokeStyle = colorx;
+        ctx.fillStyle = colorx + "33";
         ctx.stroke();
         ctx.fill();
 
         // Hiển thị thông tin nhãn và điểm số
         ctx.font = '14px Arial';
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = colorx;
+        ctx.strokeStyle = 'white'; // Màu đường viền
+        ctx.lineWidth = 2; // Độ dày của đường viền
+        ctx.strokeText(label + " " + score.toFixed(2), box[0], box[1] - 5);
         ctx.fillText(label + " " + score.toFixed(2), box[0], box[1] - 5);
       });
     //};
